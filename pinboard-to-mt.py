dfea286b4f95476fc0a3610e4fed9552e9f9a60e
Phil Gyford <phil@gyford.com>
2014-03-24 20:03:08 +0000

    Make most of the HTML

--- pinboard-to-mt.py
+++ pinboard-to-mt.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
 import ConfigParser
+import datetime
 import pinboard
 import sys
+import urllib2
 
 
 class PinboardToMT:
 
     def __init__(self, date=None):
+        if date is None:
+            date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
         self.date = date
         self.load_config()
 
@@ -18,20 +23,62 @@ class PinboardToMT:
         except IOError:
             raise PinboardToMTError("Can't read config file: " + config_file)
 
-        settings = ['pinboard_api_token', 'verbose']
+        settings = ['pinboard_api_token',
+                    'mt_blog_id',
+                    'mt_author_id',
+                    'mt_post_time',
+                    'include_tags',
+                    'verbose']
 
         for setting in settings:
             setattr(self, setting, config.get('Settings', setting))
 
     def start(self):
-        self.get_pinboard_links()
+        links = self.get_pinboard_links()
+        if len(links) == 0:
+            return
+
+        html = self.make_html(links)
+        print links 
+        print html
 
     def get_pinboard_links(self):
-        p = pinboard.open(token=self.pinboard_api_token)
+        try:
+            p = pinboard.open(token=self.pinboard_api_token)
+        except urllib2.HTTPError, error:
+            raise PinboardToMTError("Can't connect to Pinboard: %s" % error)
         print p
-        posts = p.posts(date='2014-03-16')
-        print posts
+        links = p.posts(date=self.date)
+        self.message("Fetched %s link(s) from Pinboard." % len(links))
+        return links
+
+    def make_html(self, links):
+        html = ''
+        for link in links:
+            html += "<dt><a href=\"%s\">%s</a></dt>\n<dd>" % (
+                                            link['href'], link['description']) 
+
+            if link.get('extended', False):
+                html += link['extended']
+
+            if self.include_tags:
+                tags = []
+                for tag in link.get('tags', []):
+                    tags.append(
+                        '<a href="http://pinboard.in/u:%s/t:%s">%s</a>' % (
+                                                            'TODO', tag, tag))
+                html += ' <span class="tags">%s</span>' % ', '.join(tags)
+
+            html += "</dd>\n"
+
+        html = "<dl class=\"links\">\n%s</dl>\n" % html
+        return html
+
         
+    def message(self, text):
+        "Output debugging info, if in verbose mode."
+        if self.verbose:
+            print text
 
 
 
